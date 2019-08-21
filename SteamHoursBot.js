ce569110666f4a1718c167539a93902b6b69b0b9
goodjonte <goodjonte@gmail.com>
2019-08-21 13:47:40 +1200

    Update SteamHoursBot.js
    Now asks for username and password instead of manually entering

--- SteamHoursBot.js
+++ SteamHoursBot.js
@@ -1,15 +1,19 @@
 const SteamUser = require('steam-user');
+const SteamTotp = require('steam-totp')
 const client = new SteamUser();
+const readline = require('readline-sync')
+
+let steamusername = readline.question("UserName: ")
+let steampassword = readline.question("Password: ")
 
 const logOnOptions = {
-	accountName: "",//insert user name between""
-	password: "",//insert password between""
+	accountName: steamusername,//insert user name between""
+	password: steampassword,//insert password between""
 };
 
 client.logOn(logOnOptions);
 
 client.on('loggedOn', () => {
 	client.gamesPlayed([]);//Insert steam app Ids here between [], seperate with commas
-	console.log('succesfully logged on and playing')
+	console.log('Succesfully logged on and playing')
 });
-
