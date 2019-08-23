fcd8589f11e4f509f5ec2371b7d82299b9472b7d
goodjonte <goodjonte@gmail.com>
2019-08-23 16:13:33 +1200

    Input for appIDs
    Added an input with a loop so user can input up to 32 appIDs.

--- SteamHoursBot.js
+++ SteamHoursBot.js
@@ -3,17 +3,29 @@ const SteamTotp = require('steam-totp')
 const client = new SteamUser();
 const readline = require('readline-sync')
 
-let steamusername = readline.question("UserName: ")
-let steampassword = readline.question("Password: ")
+let steam_username = readline.question("UserName: ")
+let steam_password = readline.question("Password: ")
 
 const logOnOptions = {
-	accountName: steamusername,//insert user name between""
-	password: steampassword,//insert password between""
+	accountName: steam_username,
+	password: steam_password,
 };
 
 client.logOn(logOnOptions);
 
 client.on('loggedOn', () => {
-	client.gamesPlayed([]);//Insert steam app Ids here between [], seperate with commas
+	var count = 0
+	var list_of_app_ids = []
+	while (count < 32) {
+	let appid_string = readline.question("Enter an appID or write done: ")
+	if (appid_string == "done"){
+		break;
+	}else {
+		var appid_int = parseInt(appid_string)
+		list_of_app_ids.push(appid_int)
+		var count = count + 1
+	}
+}
+	client.gamesPlayed(list_of_app_ids);
 	console.log('Succesfully logged on and playing')
 });
